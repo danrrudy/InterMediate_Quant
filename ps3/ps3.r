63ea9a36ef596c6ca3f7749bcc04fdcd11d858bc
danrrudy <danielrudy@pitt.edu>
2023-02-03 16:32:08 -0500

    completed problem set 3 code

--- ps3/ps3.r
+++ ps3/ps3.r
@@ -4,9 +4,14 @@
 #Problem Set 3
 #This file is designed to output all answers to the console via source("ps3.r")
 
-rm(list=ls())
+#rm(list=ls())
 library(foreign)
+library(haven)
+#library(rtools)
+library(ggplot2)
+library(car)
 library(marginaleffects)
+#library(linearHypothesis)
 #===functions===
 summary_stats <- function(var){
    mean <- mean(var)
@@ -22,3 +27,28 @@ header <- function(num){
 }
 
 #===end functions===
+df<-read.dta("Problem Set 3.dta")
+
+
+header(1)
+model1 <- lm(Y ~ X1 + X2 + X3,data=df)
+model2 <- lm(Y ~ X1 + X2 + X3 + X1*X3 + X2*X3,data=df)
+print(summary(model1))
+print(summary(model2))
+
+message("\n\n Linear Hypothesis")
+print(linearHypothesis(model2, c("X1:X3=0", "X2:X3=0")))
+
+#var.test(lm(X1:X3 ~ 0, data = df),lm(X2:X3~0, data = df))
+message("\n\n Marginal Effects")
+print(marginaleffects(model1, newdata = datagrid(X2=0, X3 = c(0,1), model = model1), variables = c("X1")))
+print(marginaleffects(model2, newdata = datagrid(X2=0, X3 = c(0,1), model = model2), variables = c("X1")))
+#linearHypothesis(mod)
+#linearHypothesis(mod)
+
+p = plot_cme(model2, effect = "X1", condition= list("X2", "X3" = c(0,1)),conf.level=0.9)
+X11() #This and
+plot(p)  #This together will draw the plot in a new window
+
+#var.test(lm(X1 + X2 + X3,data=df),data=df)
+#var.test(lm(X1 + X2 + X3 + X1*X3 + X2*X3,data=df),data=df)
